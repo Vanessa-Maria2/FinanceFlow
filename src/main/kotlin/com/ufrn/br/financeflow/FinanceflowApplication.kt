db33e8e1dc95da29c59c771a28f54296b3cf7f2f
Vanessa <vanessa.maria@imd.ufrn.br>
2024-08-19 22:01:25 -0300

    inicial commit

---
+++ src/main/kotlin/com/ufrn/br/financeflow/FinanceflowApplication.kt
@@ -0,0 +1,11 @@
+package com.ufrn.br.financeflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceflowApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinanceflowApplication>(*args)
+}
---
+++ src/test/kotlin/com/ufrn/br/financeflow/FinanceflowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ufrn.br.financeflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinanceflowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
