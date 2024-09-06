b1e9e5eb40d97fbe5852edcc776e0f45a494200a
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2024-09-05 21:10:39 -0300

    feat: Setup basic Home screen

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,46 @@
 import {
   Card,
-  CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { MoneyReceiveSquareIcon, MoneySendSquareIcon, AddCircleHalfDotIcon } from "hugeicons-react"
+import { Button } from "@/components/ui/button"
+import { DialogCreateRecord } from "@/components/dialog-create-record"
+import { TableRecords } from "@/components/table-records"
 
 
 export default function Home() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter>
-    </Card>
+    <div>
+      <div className="flex justify-between mb-8">
+        <div className="flex gap-3">
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex items-center gap-2">
+                <MoneyReceiveSquareIcon className="text-teal-500 " />
+                R$ 500
+              </CardTitle>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex items-center gap-2">
+                <MoneySendSquareIcon className="text-rose-500 " />
+                R$ 700
+              </CardTitle>
+            </CardHeader>
+          </Card>
+        </div>
+
+        <div>
+          <DialogCreateRecord />
+        </div>
+      </div>
+
+
+      <TableRecords />
+
+    </div>
   )
 }
---
+++ frontend/src/components/button-create-record.tsx
@@ -0,0 +1,11 @@
+import { AddCircleHalfDotIcon } from "hugeicons-react"
+import { Button } from "@/components/ui/button"
+
+
+export function ButtonCreateRecord() {
+    return (
+        <Button variant='default'>
+            <AddCircleHalfDotIcon />
+        </Button>
+    )
+}
