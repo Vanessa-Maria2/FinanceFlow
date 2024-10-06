a780aa3ea893b9e59a9fdcd04dbfd0fdcb1dc3d1
Vanessa-Maria2 <vanessa.silva5205@gmail.com>
2024-10-05 23:00:20 -0300

    feat: implemented financial recording. #7

--- frontend/src/components/dialog-create-record.tsx
+++ frontend/src/components/dialog-create-record.tsx
@@ -1,21 +1,52 @@
+'use client';
 import { Button } from "@/components/ui/button"
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { ButtonCreateRecord } from '@/components/button-create-record'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 
 export function DialogCreateRecord() {
+  const [value, setValue] = useState("");
+  const [isIncome, setIsIncome] = useState(true);
+
+  const handleSubmit = async () => {
+    const record = {
+      amount: parseFloat(value),
+      type: isIncome ? "INCOME" : "EXPENSE",
+      data: new Date(),
+    };
+
+    try {
+      const response = await fetch('http://localhost:8080/finance-record', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(record),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao cadastrar o registro');
+      }
+
+      const result = await response.json();
+      console.log('Registro cadastrado com sucesso:', result);
+      setValue(""); 
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -23,35 +54,37 @@ export function DialogCreateRecord() {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[525px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Register</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Create new finance record
           </DialogDescription>
         </DialogHeader>
         <Tabs defaultValue="account" className="grid gap-4 py-4">
             <TabsList className="grid w-full grid-cols-2">
-                <TabsTrigger value="account" className="text-teal-300">
-                    <p className="text-teal-500">Income</p>
+                <TabsTrigger value="account" className="text-teal-300" onClick={() => setIsIncome(true)}>
+                    <p className="text-teal-500">INCOME</p>
                 </TabsTrigger>
-                <TabsTrigger value="password">
-                    <p className="text-rose-500">Expense</p>
+                <TabsTrigger value="password" onClick={() => setIsIncome(false)}>
+                    <p className="text-rose-500">EXPENSE</p>
                 </TabsTrigger>
             </TabsList>
             <TabsContent value="account">
                 <div className="space-y-1 flex items-center gap-3">
                     <Label htmlFor="value">R$</Label>
-                    <Input id="value" type="real" placeholder="10.00" />
+                    <Input id="value" type="real" placeholder="10.00" value={value}
+                      onChange={(e) => setValue(e.target.value)}/>
                 </div>
             </TabsContent>
             <TabsContent value="password">
             <div className="space-y-1 flex items-center gap-3">
                     <Label htmlFor="value">R$</Label>
-                    <Input id="value" type="real" placeholder="10.00" />
+                    <Input id="value" type="real" placeholder="10.00" value={value}
+                      onChange={(e) => setValue(e.target.value)}/>
                 </div>
             </TabsContent>
         </Tabs>
 
-        <Button>Create</Button>
+        <Button onClick={handleSubmit}>Create</Button>
       </DialogContent>
     </Dialog>
   )
