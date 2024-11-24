169ce439b93f5357ebc5e81b736380c4e1f78aa5
Vanessa-Maria2 <vanessa.silva5205@gmail.com>
2024-11-24 13:49:33 -0300

    feat: implementing the function to delete a financial record. #8

--- frontend/src/components/table-records.tsx
+++ frontend/src/components/table-records.tsx
@@ -1,103 +1,123 @@
 'use client';
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import { useState, useEffect } from "react";
 import { FinanceType } from "./models/financeType";
 import { ApiResponse } from "./models/apiResponse";
 import { CategoryType } from "./models/categoryType";
 import { Delete02Icon, EditUser02Icon } from "hugeicons-react";
 import { Button } from "@/components/ui/button"
-  
-  export function TableRecords() {
-    const [finances, setFinances] = useState<FinanceType[]>([]);
-    const [totalAmount, setTotalAmount] = useState<number>(0);
 
-    useEffect(() => {
+export function TableRecords() {
+  const [finances, setFinances] = useState<FinanceType[]>([]);
+  const [totalAmount, setTotalAmount] = useState<number>(0);
 
-      const financesType = async () => {
-        try {
-          const response = await fetch('http://localhost:8080/finance-record');
-          if(!response.ok) {
-            throw new Error('Error ao buscar as finanças')
-          }
-          const data: ApiResponse<FinanceType> = await response.json();
+  useEffect(() => {
+    fetchFinances();
+  }, []);
 
-          const mappedData: FinanceType[] = data.items.map((finance) => ({
-            id: finance.id,
-            type: finance.type,
-            amount: Number(finance.amount),
-            typeCategories: finance.typeCategories.map((category: CategoryType) => ({
-              id: category.id,
-              name: category.name,
-            })),
-            description: finance.description,
-            date: new Date(finance.date)
-          }));
-          
-          const total = mappedData.reduce((acc, finance) => acc + finance.amount, 0);
-          setTotalAmount(total);
-          setFinances(mappedData);        
-        } catch(error) {
-          console.error('Error: ', error);
-        }
-      };
-      financesType();
-    }, []);
+  const fetchFinances = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/finance-record');
+      if (!response.ok) {
+        throw new Error('Error ao buscar as finanças')
+      }
+      const data: ApiResponse<FinanceType> = await response.json();
 
-    return (
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Id</TableHead>
-            <TableHead className="w-[100px]">Description</TableHead>
-            <TableHead>Type</TableHead>
-            <TableHead>Date</TableHead>
-            <TableHead>Categories</TableHead>
-            <TableHead>Amount</TableHead>
-            <TableHead className="text-right">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-        {finances.map((finance) => (
-            <TableRow key={finance.id}>
-              <TableCell className="font-medium">{finance.id}</TableCell>
-              <TableCell>{finance.description}</TableCell>
-              <TableCell>{finance.type}</TableCell>
-              <TableCell>{finance.date.toLocaleDateString('pt-BR')}</TableCell>
-              <TableCell>{finance.typeCategories.map((category) => category.name).join(', ')}</TableCell>
-              <TableCell>{finance.amount}</TableCell>
-              <TableCell className="text-right">
-                <Button style={{ background: 'transparent' }}>
-                  <Delete02Icon 
-                    size={24} 
-                    color={"#d0021b"}
-                  />
-                </Button>
-                <Button style={{ background: 'transparent' }}> 
-                  <EditUser02Icon 
-                    size={24} 
-                    color={"#6bb833"}
-                  />
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={6}>Total</TableCell>
-            <TableCell className="text-right">{totalAmount}</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
-    )
+      const mappedData: FinanceType[] = data.items.map((finance) => ({
+        id: finance.id,
+        type: finance.type,
+        amount: Number(finance.amount),
+        typeCategories: finance.typeCategories.map((category: CategoryType) => ({
+          id: category.id,
+          name: category.name,
+        })),
+        description: finance.description,
+        date: new Date(finance.date)
+      }));
+
+      const total = mappedData.reduce((acc, finance) => acc + finance.amount, 0);
+      setTotalAmount(total);
+      setFinances(mappedData);
+    } catch (error) {
+      console.error('Error: ', error);
+    }
   }
-  
+
+  const deleteFinance = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:8080/finance-record/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao cadastrar o registro');
+      }
+
+      console.log("deletado com sucesso")
+      fetchFinances();
+
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Id</TableHead>
+          <TableHead className="w-[100px]">Description</TableHead>
+          <TableHead>Type</TableHead>
+          <TableHead>Date</TableHead>
+          <TableHead>Categories</TableHead>
+          <TableHead>Amount</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {finances.map((finance) => (
+          <TableRow key={finance.id}>
+            <TableCell className="font-medium">{finance.id}</TableCell>
+            <TableCell>{finance.description}</TableCell>
+            <TableCell>{finance.type}</TableCell>
+            <TableCell>{finance.date.toLocaleDateString('pt-BR')}</TableCell>
+            <TableCell>{finance.typeCategories.map((category) => category.name).join(', ')}</TableCell>
+            <TableCell>{finance.amount}</TableCell>
+            <TableCell className="text-right">
+              <Button style={{ background: 'transparent' }} onClick={() => deleteFinance(finance.id)}>
+                <Delete02Icon
+                  size={24}
+                  color={"#d0021b"}
+                />
+              </Button>
+              <Button style={{ background: 'transparent' }}>
+                <EditUser02Icon
+                  size={24}
+                  color={"#6bb833"}
+                />
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={6}>Total</TableCell>
+          <TableCell className="text-right">{totalAmount}</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  )
+}
