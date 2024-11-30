821088d78efc2629947f01f754ea518f36a192fe
Vanessa-Maria2 <vanessa.silva5205@gmail.com>
2024-11-29 21:11:31 -0300

    feat: added description field and date field

--- frontend/src/components/dialog-create-record.tsx
+++ frontend/src/components/dialog-create-record.tsx
@@ -14,7 +14,16 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectGroup, SelectLabel, SelectValue } from "./ui/select";
+import { CalendarIcon } from "lucide-react";
 import { CategoryType } from "./models/categoryType";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { format } from "date-fns"
+import { cn } from "@/lib/utils"
 
 
 export function DialogCreateRecord() {
@@ -22,12 +31,13 @@ export function DialogCreateRecord() {
   const [isIncome, setIsIncome] = useState(true);
   const [categoryTypes, setCategoryTypes] = useState<CategoryType[]>([]);
   const [selectedCategories, setSelectedCategories] = useState<CategoryType[]>([]);
+  const [date, setDate] = React.useState<Date>()
+  const [description, setDescription] = useState("");
 
   useEffect(() => {
     const fetchCategoryTypes = async () => {
       try {
-        const response = await fetch('http://localhost:8080/type-category'); 
-        console.log(response)
+        const response = await fetch('http://localhost:8080/type-category');
         if (!response.ok) {
           throw new Error('Erro ao buscar tipos de categoria');
         }
@@ -56,8 +66,9 @@ export function DialogCreateRecord() {
     const record = {
       amount: parseFloat(value),
       type: isIncome ? "INCOME" : "EXPENSE",
-      data: new Date(),
+      data: date,
       typeCategories: selectedCategories,
+      description: description
     };
 
     try {
@@ -74,8 +85,6 @@ export function DialogCreateRecord() {
       }
 
       const result = await response.json();
-      console.log('Registro cadastrado com sucesso:', result);
-      setValue("");
     } catch (error) {
       console.error('Erro:', error);
     }
@@ -118,7 +127,9 @@ export function DialogCreateRecord() {
           </TabsContent>
           <Select>
             <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Select a type category" />
+              <SelectValue placeholder="Select a type category"> {selectedCategories.length > 0
+                ? selectedCategories.map((category) => category.name).join(', ')  // Exibe as categorias separadas por vírgula
+                : "Select a type category"}</SelectValue>
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
@@ -126,21 +137,43 @@ export function DialogCreateRecord() {
                 {categoryTypes.map((category) => (
                   <SelectItem
                     key={category.id}
-                    value={category.id}
+                    value={category.id.toString()}
                     onClick={() => handleSelectCategory(category)}
                   >
-                    <input
-                      type="checkbox"
-                      checked={selectedCategories.some((c) => c.id === category.id)}
-                      readOnly
-                      className="mr-2"
-                    />
                     {category.name}
                   </SelectItem>
                 ))}
               </SelectGroup>
             </SelectContent>
           </Select>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[280px] justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date ? format(date, "PPP") : <span>Pick a date</span>}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0 popover-content pointer-events-auto">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+
+          <div className="space-y-1 flex items-center gap-3">
+            <Label htmlFor="description">Description</Label>
+            <Input id="description" type="text" placeholder="Digite..." value={description}
+              onChange={(e) => setDescription(e.target.value)} />
+          </div>
         </Tabs>
 
         <Button onClick={handleSubmit}>Create</Button>
